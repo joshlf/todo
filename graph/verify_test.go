6ef43005718969076cfa13687e6195d072096f72
Kelvin Jackson <kechpaja@comcast.net>
2014-01-25 16:06:06 -0500

    Test for verify finished

--- graph/verify_test.go
+++ graph/verify_test.go
@@ -5,16 +5,8 @@ import (
 )
 
 func TestGraphFromTasks(t *testing.T) {
-	tasks := []Task{
-	Task{Id: "A", Dependencies: TaskIDSet{"B": void, "C": void}},
-	Task{Id: "B", Dependencies: TaskIDSet{"D": void, "E": void}},
-	Task{Id: "C", Dependencies: TaskIDSet{"E": void, "F": void}},
-	Task{Id: "D", Dependencies: TaskIDSet{}},
-	Task{Id: "E", Dependencies: TaskIDSet{}},
-	Task{Id: "F", Dependencies: TaskIDSet{}}}
-
-	t1, _ := GraphFromTasks(tasks)
 	t2 := makeTestTasks()
+	t1, _ := GraphFromTasks(makeTestTaskSlice())
 	if !tasksEqual(t1, t2) {
 		t.Errorf("Tasks are not equal.")
 	}
