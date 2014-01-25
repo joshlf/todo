13be20cd91b5b672c90cafbfe8e250a9b030943b
Kelvin Jackson <kechpaja@comcast.net>
2014-01-24 22:29:45 -0500

    First version of (un)blocked functions

---
+++ unblocked.go
@@ -0,0 +1,31 @@
+package todo
+
+// TODO this implementation could probably be improved 
+// to be a bit more efficient. 
+
+// Returns set of blocked tasks
+func Blocked(t Tasks) Tasks {
+	return Filter(t, func(id TaskID, task *Task) bool {
+		return !unblocked(id, task, t)
+	})
+}
+
+// Returns set of unblocked tests
+func Unblocked(t Tasks) Tasks {
+	return Filter(t, func(id TaskID, task *Task) bool {
+		return unblocked(id, task, t)
+	})
+}
+
+// determine if a task is not blocked (has no unfinished dependencies) 
+func unblocked(id TaskID, task *Task, t Tasks) bool {
+	if !task.Completed {
+		for _, taskid := range task.Depends {
+			if !t[taskid].Completed {
+				return false
+			}
+		}
+	}
+
+	return true
+}
