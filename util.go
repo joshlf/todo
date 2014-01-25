884874668061feb6bdbae0eab789c53d8501cc8a
Joshua Liebow-Feeser <hello@joshlf.com>
2014-01-25 12:51:29 -0500

    Added json file -> graph.Tasks and graph.Tasks -> json file utilities.

---
+++ util.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/joshlf13/todo/graph"
+	"github.com/joshlf13/todo/json"
+	"io/ioutil"
+	"os"
+)
+
+func jsonFileToTasksDefault() (graph.Tasks, error) { return jsonFileToTasks(file) }
+func tasksToJSONFileDefault(t graph.Tasks) error   { return tasksToJSONFile(file, t) }
+
+func jsonFileToTasks(fname string) (graph.Tasks, error) {
+	f, err := os.Open(fname)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	tasks, err := json.Unmarshal(b)
+	if err != nil {
+		return nil, err
+	}
+	t, err := graph.GraphFromTasks(tasks)
+	if err != nil {
+		return nil, err
+	}
+	return t, err
+}
+
+func tasksToJSONFile(t graph.Tasks, fname string) error {
+	b, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(fname, b, 0)
+}
