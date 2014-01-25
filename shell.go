01373c0fb719b8ef7afb7114047545c450c7b072
Joshua Liebow-Feeser <hello@joshlf.com>
2014-01-25 03:25:53 -0500

    Added error reporting to shell.go; removed shell_test.go

--- shell.go
+++ shell.go
@@ -1,6 +1,7 @@
 package todo
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -12,13 +13,13 @@ func EditString(s string) string {
 	// create temporary file
 	f, err := ioutil.TempFile("", "todo-")
 	if err != nil {
-		panic("todo: EditString: failed to create temporary file.")
+		panic(fmt.Sprintf("todo: EditString: failed to create temporary file: %v", err))
 		// TODO this should be a real error eventually
 	}
 
-	_, werr := f.WriteString(s)
-	if werr != nil {
-		panic("todo: EditString: failed to write to temporary file.")
+	_, err = f.WriteString(s)
+	if err != nil {
+		panic(fmt.Sprintf("todo: EditString: failed to write to temporary file: %v", err))
 		// TODO change this later
 	}
 
@@ -26,21 +27,21 @@ func EditString(s string) string {
 	fname := f.Name()
 
 	// call editor
-	if exec.Command(GetEditor(), fname).Run() != nil {
-		panic("todo: EditString: failed to launch editor.")
+	if err = exec.Command(GetEditor(), fname).Run(); err != nil {
+		panic(fmt.Sprintf("todo: EditString: failed to launch editor: %v", err))
 		// TODO change this later
 	}
 
 	// read file
-	b, rferr := ioutil.ReadFile(fname)
-	if rferr != nil {
-		panic("todo: EditString: failed to grab contents of tmp file.")
+	b, err := ioutil.ReadFile(fname)
+	if err != nil {
+		panic(fmt.Sprintf("todo: EditString: failed to grab contents of tmp file: %v", err))
 		// TODO change this later
 	}
 
 	// delete file
-	if os.Remove(fname) != nil {
-		panic("todo: EditString: failed to remove tmp file.")
+	if err = os.Remove(fname); err != nil {
+		panic(fmt.Sprintf("todo: EditString: failed to remove tmp file: %v", err))
 		// TODO change this later, and is very hacky!
 	}
 
